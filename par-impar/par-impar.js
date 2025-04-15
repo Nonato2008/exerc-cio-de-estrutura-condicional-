7bbf1da581fd3e82f03a31fa6a3aee8209329972
unknown <silvabryan203@gmail.com>
2025-04-15 10:39:34 -0300

    Primeiro commit

--- par-impar/par-impar.js
+++ par-impar/par-impar.js
@@ -4,9 +4,9 @@ if (isNaN (num)){ //código para identificar se o usúario não escreveu letras 
     alert("Esse número não é válido!")
     
 } else {
-    if (num % 2 == 0){ // se a sobra da divisão for igual à 0
+    if (num % 2 == 0){ // se a sobra da divisão for igual à 0, exibira o seguinte alerta
         alert ("Este número é par!")
-    } else {
+    } else { //se a sobra da divisão do número não for 0, exibirá este alerta
         alert ("Este número é impar!")
     } 
 }   
--- positivo_negativo/positivo.js
+++ positivo_negativo/positivo.js
@@ -2,12 +2,12 @@ let num = parseInt(prompt("Digite um número:")); //declarar váriavel que será
 
 if (isNaN(num)) { //código para identificar se o usúario não escreveu letras ao invés de números
     alert("Error este número não é válido! Digite apenas números.")
-} else {
+} else { //se não tiver letras irá continuar ocódigo
 
 
-    if (num > 0) {
+    if (num > 0) { //se o número for maior que zero aparecerá o seguinte alerta abaixo
         alert("Este número é positivo")
-    } else {
+    } else { // senão for maior que zero será zero ou negativo
         if (num == 0) {
             alert("Este número é igual zero")
         } else {
